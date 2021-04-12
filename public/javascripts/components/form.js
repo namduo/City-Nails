8ad8f4a16093fb0af44ede4f29b4d4bbfd9b5f82
Nam Nguyen <nam.nguyen@secretescapes.com>
2021-04-12 01:46:20 +0100

    email js update

--- public/javascripts/components/form.js
+++ public/javascripts/components/form.js
@@ -1,40 +1,45 @@
 export default function formSubmission(){
-   $('#contact-form').submit(function(e) {
+  $('#contact-form').submit(function(e) {
 
-      e.preventDefault();
-      
-      // Form Validate
-      if (
-            $('#name-field').val() === '' || 
-            $('#email-field').val() === '' || 
-            $('#message-field').val() === '' || 
-            $('#phone-field').val() === '' ){  
-               alert('Please Fill All Fields')
-            } else {
-            
-         $.ajax({
-            type: 'post',
-            url: '/contact',
-            data: {
-               name: $('#name-field').val(),
-               email: $('#email-field').val(),
-                  message: $('#message-field').val(),
-                  phone: $('#phone-field').val()
-               },
-               
-            success: function() {
-               // Clear the form
-               $('#name-field').val('');
-               $('#email-field').val('');
-               $('#message-field').val('');
-               $('#phone-field').val('');
-      
-               // Success Message
-               $('#form-message').append('Message sent! We\'ll get back to you as soon as we can.');
-               }
-               
-            });
-         }
+  e.preventDefault();
+  
+  // Form Validate
+  if (
+    $('#name-field').val() === '' || 
+    $('#email-field').val() === '' || 
+    $('#message-field').val() === '' || 
+    $('#phone-field').val() === '' ){  
+        alert('Please Fill All Fields')
+    } else {
+    
+    const data = {
+      service_id: 'citynails_gmail_19',
+      template_id: 'template__2',
+      user_id: 'user_eE3zF4XZczCQxqFROwutS',
+      template_params: {
+        email: $('#email-field').val(),
+        from_name: $('#name-field').val(),
+        phone: $('#email-field').val(),
+        message_html: $('#message-field').val()
+      }
+    }
+    
+    $.ajax({
+      type: 'post',
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      url: 'https://api.emailjs.com/api/v1.0/email/send',
+      success: function() {
+        // Clear the form
+        $('#name-field').val('');
+        $('#email-field').val('');
+        $('#message-field').val('');
+        $('#phone-field').val('');
+
+        // Success Message
+        $('#form-message').append('Message sent! We\'ll get back to you as soon as we can.');
+      }  
       });
+    }
+  });
 }
-
